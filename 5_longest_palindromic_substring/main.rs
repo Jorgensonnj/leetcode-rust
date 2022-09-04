28cccc8f5dd6a6912628e21cc8ec0a79f5cb5f6c
Nicholas Jorgenson <jorgensonnj@gmail.com>
2022-09-04 19:18:14 -0400

    found longest palindrome in a string

---
+++ 5_longest_palindromic_substring/main.rs
@@ -0,0 +1,146 @@
+use std::time::Instant;
+
+// problem
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+// my original code
+pub fn my_longest_palindrome(s: String) -> String {
+    if s.len() < 2 { return s; }
+    let string: Vec<char> = s.chars().collect();
+
+    let mut start_ptr = 0;
+    let mut end_ptr = 0;
+
+    for index in 0..string.len() {
+        // odd
+        let mut left = index;
+        let mut right = index;
+        while ( left > 0 || left == 0 ) && right < string.len() {
+            if string[ left ] == string[ right ] {
+                if right - left > end_ptr - start_ptr {
+                    start_ptr = left;
+                    end_ptr = right;
+                }
+
+                if left == 0 { break; }
+
+                left -= 1;
+                right += 1;
+            } else {
+                break;
+            }
+        }
+
+        // even
+        left = index;
+        right = index + 1;
+        while ( left > 0 || left == 0 ) && right < string.len() {
+            if string[ left ] == string[ right ] {
+                if right - left > end_ptr - start_ptr {
+                    start_ptr = left;
+                    end_ptr = right;
+                }
+
+                if left == 0 { break; }
+
+                left -= 1;
+                right += 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return string[start_ptr..end_ptr + 1].iter().collect();
+}
+
+// someone else's code
+// https://leetcode.com/problems/longest-palindromic-substring/discuss/1210849/Rust
+pub fn other_longest_palindrome(s: String) -> String {
+    fn is_palidrone(s: &[u8]) -> bool {
+        // Iterate left to right along with iterating from right to left,
+        // make sure each spot is the same.
+        // Returns false once the left is not equal to the right
+        s.iter().zip(s.iter().rev()).all(|(l, r)| l == r)
+    }
+
+    for size in (1..=s.len()).rev() {
+        match s.as_bytes()
+            .windows(size)
+            .find(|substr| is_palidrone(substr)) {
+            Some(pal) => return String::from_utf8(pal.to_vec()).unwrap(),
+            None => continue,
+        }
+    }
+    // No palidrone found
+    String::from("")
+}
+
+fn main() {
+
+    // test 1
+    let s1 = "1211111".to_string();
+    let s2 = s1.clone();
+
+    // test 2
+    //let s1 = "semjhjlritnjgapzrakcqahaqetwllrldktufvdgkfusniv".to_string();
+    //let s2 = s1.clone();
+
+    // test 3
+    //let s1 = "a".to_string();
+    //let s2 = s1.clone();
+
+    // test 3
+    //let s1 = "bb".to_string();
+    //let s2 = s1.clone();
+
+    let my_time = Instant::now();
+    let my_result = my_longest_palindrome( s1 );
+    println!("Result: {:?}, Time: {:?}", my_result, my_time.elapsed());
+
+    let other_time = Instant::now();
+    let other_result = other_longest_palindrome( s2 );
+    println!("Result: {:?}, Time: {:?}", other_result, other_time.elapsed());
+}
+
+// worked, but it was not fast enough
+//pub fn my_longest_palindrome(s: String) -> String {
+//
+//    let mut start_prt = 0;
+//    let mut end_prt = 0;
+//    let mut length = 0;
+//    for i in 0..s.len() {
+//        for j in i..s.len() {
+//            let slice = &s[i..j+1];
+//            if is_palindrome(slice) {
+//                if slice.len() > length {
+//                    length = slice.len();
+//                    start_prt = i;
+//                    end_prt = j + 1;
+//                }
+//            }
+//        };
+//    };
+//
+//
+//    return s[start_prt..end_prt].to_string();
+//}
+//
+//pub fn is_palindrome(s: &str) -> bool {
+//    let string: Vec<_> = s.chars().collect();
+//    let mut high = string.len();
+//    if high == 0 {
+//        return false;
+//    };
+//
+//    let mut low = 0;
+//    while low < high {
+//        if string[low] != string[high - 1] {
+//            return false;
+//        };
+//        low += 1;
+//        high -= 1;
+//    };
+//
+//    return true;
+//}
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ Avaliable binaries:
     - 1_two_sum
     - 2_add_two_numbers
     - 3_longest_substring_without_repeating_characters
+    - 4_median_of_two_sorted_arrays
+    - 5_longest_palindromic_substring
 ";
 
     println!("{}", directions);
