d452545ecb72d51b0da5d5b58d0c4d82f3573ac5
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2024-02-18 14:10:42 -0500

    renamed repo and minor changes to solution 200

--- 200_number_of_islands/main.rs
+++ 200_number_of_islands/main.rs
@@ -11,14 +11,16 @@ pub fn my_num_islands(grid: Vec<Vec<char>>) -> i32 {
     let mut count = 0;
     for i in 0..grid.len() as i32 {
         for j in 0..grid[0].len() as i32 {
-            if depth_first(&grid, i, j, &mut visited) {
-                //println!("y: {}, x: {}", i, j);
-                count += 1;
+            if grid[i as usize][j as usize] == '1' { 
+                if depth_first(&grid, i, j, &mut visited) {
+                    //println!("y: {}, x: {}", i, j);
+                    count += 1;
+                };
             };
         }
     }
 
-    return count;
+    count
 }
 
 pub fn depth_first(grid: &Vec<Vec<char>>, row: i32, column: i32, visited: &mut HashSet<String> ) -> bool {
@@ -42,7 +44,7 @@ pub fn depth_first(grid: &Vec<Vec<char>>, row: i32, column: i32, visited: &mut H
     depth_first(grid, row + 1, column,     visited);
     depth_first(grid, row,     column + 1, visited);
 
-    return true;
+    true
 }
 
 //https://leetcode.com/problems/number-of-islands/solutions/2105704/rust-iterative-dfs-bfs-with-comments
@@ -52,15 +54,20 @@ pub fn other_num_islands(mut grid: Vec<Vec<char>>) -> i32 {
     let mut stack = vec![];
     let (m, n) = (grid.len(), grid[0].len());
 
-    for i in 0..m {
-        for j in 0..n {
-            if grid[i][j] == '1' {
-                stack.push((i, j));
-                while let Some((r, c)) = stack.pop() {
-                    if r < m && c < n && grid[r][c] == '1' {
-                        grid[r][c] = '0';
-                        for rc in [0, 1, 0, !0, 0].windows(2) {
-                            stack.push((r.wrapping_add(rc[0]), c.wrapping_add(rc[1])));
+    for row in 0..m {
+        for column in 0..n {
+            if grid[row][column] == '1' {
+                stack.push((row, column));
+                while let Some((island_row, island_column)) = stack.pop() {
+                    if island_row < m &&
+                       island_column < n &&
+                       grid[island_row][island_column] == '1'
+                    {
+                        grid[island_row][island_column] = '0';
+                        for window in [0, 1, 0, !0, 0].windows(2) {
+                            stack.push(
+                                (island_row.wrapping_add(window[0]), island_column.wrapping_add(window[1]))
+                            );
                         }
                     }
                 }
